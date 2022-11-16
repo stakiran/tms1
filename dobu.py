86d0789493546ab1683a2bbd0c5c5e3f90ad2d07
stakiran <stakiran@users.noreply.github.com>
2022-11-16 17:54:50 +0900

    add command template.

---
+++ dobu.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+def file2list(filepath):
+    ret = []
+    with open(filepath, encoding='utf8', mode='r') as f:
+        ret = [line.rstrip('\n') for line in f.readlines()]
+    return ret
+
+def list2file(filepath, ls):
+    with open(filepath, encoding='utf8', mode='w') as f:
+        f.writelines(['{:}\n'.format(line) for line in ls] )
+
+def parse_arguments():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument('-i', '--input-path', default=None)
+    parser.add_argument('--input-from-cd', default=False, action='store_true')
+
+    parser.add_argument('--to-dobu', default=True, action='store_true')
+    parser.add_argument('--to-html', default=False, action='store_true')
+
+    parser.add_argument('--not-stdout-but-file', default=False, action='store_true')
+
+    parser.add_argument('--html-template', default=None)
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_arguments()
